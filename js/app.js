e125c8f51ec4818cf3fe66ce14cc486360399c90
vanduc2514 <52255055+vanduc2514@users.noreply.github.com>
2020-03-07 23:48:59 +0700

    Sửa giao diện và bổ sung tính năng

--- js/app.js
+++ js/app.js
@@ -2,14 +2,25 @@ const BATTERY_LEVEL = 100;
 const BATTERY_CONSUME = 10;
 const BATTERY_OUT = 0;
 
-let MobilePhone = function () {
+let MobilePhone = function (name) {
+    this.name = name;
     this.battery = BATTERY_LEVEL;
     this.message;
     this.inbox;
     this.outbox;
     this.state = false;
     this.getBattery = function () {
+        if (this.battery < 0) {
+            this.battery = BATTERY_OUT;
+        }
         return this.battery;
+    };
+    this.getState = function () {
+        if (this.battery === BATTERY_OUT) {
+            this.state = false;
+            return this.autoTurnoff();
+        }
+        return this.state;
     };
     this.getMessage = function () {
         return this.message;
@@ -20,11 +31,7 @@ let MobilePhone = function () {
     this.getOutbox = function () {
         return this.outbox;
     };
-    this.getState = function () {
-        return this.state;
-    };
     this.writeMessage = function (message) {
-        this.getState();
         if (this.getState()) {
             this.message = message;
             this.battery -= BATTERY_CONSUME;
@@ -32,8 +39,8 @@ let MobilePhone = function () {
     };
     this.sendMessage = function (object) {
         if (this.getState()) {
-            this.outbox = this.message;
-            object.inbox = this.message;
+            this.outbox = this.getMessage();
+            object.inbox = this.getMessage();
             this.battery -= BATTERY_CONSUME;
             object.battery -= BATTERY_CONSUME;
         }
@@ -48,12 +55,82 @@ let MobilePhone = function () {
         if (this.getBattery() < BATTERY_LEVEL) {
             return this.battery += BATTERY_CONSUME;
         }
-        return this.battery;
     };
     this.autoTurnoff = function () {
-        if (this.BATTERY_LEVEL === BATTERY_OUT) {
-            this.state = false;
+        turnOff(this);
+    };
+};
+
+let pixel = new MobilePhone("pixel");
+let samsung = new MobilePhone("samsung");
+
+function turnOn(object) {
+    object.turnOn();
+    document.getElementById(object.name + '-image').src = "img/" + object.name + "_on.png";
+}
+
+function turnOff(object) {
+    object.turnOff();
+    document.getElementById(object.name + '-image').src = "img/" + object.name + "_off.png";
+    document.getElementById(object.name + '-text').style.display = "none";
+    document.getElementById(object.name + '-send').style.display = "none";
+}
+
+function changeState(object) {
+    if (object.getState()) {
+        turnOff(object);
+    } else {
+        turnOn(object);
+    }
+}
+
+function writeMessage(object) {
+    if (object.getState()) {
+        if (document.getElementById(object.name + '-text').style.display === "inline-block") {
+            document.getElementById(object.name + '-text').style.display = "none";
+            document.getElementById(object.name + '-send').style.display = "none";
+        } else {
+            document.getElementById(object.name + '-text').style.display = "inline-block";
+            document.getElementById(object.name + '-send').style.display = "inline-block";
         }
     }
+}
 
-};
+function sendMessage(object) {
+    let message = document.getElementById(object.name + '-text').value;
+    if (message !== "") {
+        object.writeMessage(message);
+        if (object.name === "samsung") {
+            return object.sendMessage(pixel);
+        }
+        return object.sendMessage(samsung);
+
+    }
+}
+
+function checkInbox(object) {
+    if (object.getState()) {
+        if (object.getInbox() !== undefined) {
+            return alert(object.getInbox());
+        }
+        return alert("Hộp thư đến trống");
+    }
+}
+
+function checkOutbox(object) {
+    if (object.getState()) {
+        if (object.getOutbox() !== undefined) {
+            return alert(object.getOutbox());
+        }
+        return alert("Hộp thư đi trống");
+    }
+}
+
+function checkBat(object) {
+    alert("Phần trăm pin hiện tại: " + object.getBattery() + " %");
+}
+
+function recharge(object) {
+    object.recharge();
+}
+
